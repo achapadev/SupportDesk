c1b55247e4c0e28c92218761f0f547513816e9d8
Adrian <achapa@adrianchapa.dev>
2022-02-09 03:55:33 -0600

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 // import to be able to use environment variables
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const res = require('express/lib/response');
 const PORT = process.env.PORT || 8000;
 
 // Connect to database
@@ -22,6 +24,21 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
